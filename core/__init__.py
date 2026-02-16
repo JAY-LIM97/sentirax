d4610f50e96bd78adaa8a832612ebbf1dc900a5c
JAY-LIM97 <ijh4u24@gmail.com>
2026-02-16 23:23:58 +0900

    커밋 내용 추가

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,5 @@
 # core/__init__.py
-#from .config import Config
+from .config import Config
 from .news_collector import NewsCollector
 from .sentiment_analyzer import SentimentAnalyzer
 from .macro_collector import MacroDataCollector
